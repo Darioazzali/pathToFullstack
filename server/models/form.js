7f754edc1b537efbe752a866c00c98b7f19ed10f
dario1989a <da.azzali@gmail.com>
2022-04-09 16:36:57 +0200

    Updated server

--- server/models/form.js
+++ server/models/form.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const formSchema = new Schema ({
+const Product = new Schema ({
     address: { type: String, required: true},
     color: {type: String, required: true},
     type: {type: String, required: true}
 });
 
-module.exports = mongoose.model("clitotem", formSchema)
+module.exports = mongoose.model("clitotem", Product)
